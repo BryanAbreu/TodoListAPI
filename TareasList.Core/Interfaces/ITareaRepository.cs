d2dc5ff184b88f4395698948c25f0b3522a18316
Bryan Abreu <batabreu13@gmail.com>
2021-02-11 19:53:43 -0400

    conection database, independency injection

---
+++ TareasList.Core/Interfaces/ITareaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TareasList.Core.Entities;
+
+namespace TareasList.Core.Interfaces
+{
+    public interface ITareaRepository
+    {
+        Task<IEnumerable<Tareas>> GetTareas();
+    }
+}
